b1a8d1a394bc1e4e811ac5e635e30cd773aa0288
Malika-Nz <Malika12345678love@gmail.com>
2023-03-02 19:01:46 +0300

    мяу

---
+++ FRONTEND/script.js
@@ -0,0 +1,8 @@
+function printData() {
+    value = document.getElementById("you_gender").value;
+    if (value == 0) {
+        alert('Вы не до конца заполнили поля')
+    } else {
+        alert("Ты зарегистрировался :)))")
+    }
+}
