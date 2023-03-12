09b17614c8be304a0dea217d7b6803f8a3551a56
Azat-Bilalov <52991059+Azat-Bilalov@users.noreply.github.com>
2023-03-12 19:35:24 +0300

    тестовый сервак

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,24 +1,25 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-var bodyParser = require('body-parser')
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded());
 app.use(express.json());
 
-app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, '../FRONTEND')))
 
+const multer = require('multer');
 
-// app.use(express.static(path.join(__dirname, '../FRONTEND')))
 app.get('/', (req, res) => {
-    console.log(req.body)
-    res.send({ message: req.body });
+    console.log(req.query)
+    res.send({ message: req.query });
 });
 
-app.post('/signup', function(req, res) {
-    res.send({ message: req.body });
+app.post('/signup', multer().fields([]), function(req, res) {
+    const obj = JSON.parse(JSON.stringify(req.body));
+    console.log(obj);
+    res.send(JSON.stringify(req.body));
 });
 
 app.listen(3333, () => {
-    console.log('Application listening on port 3333!');
+    console.log('Application listening on port 3333');
 });
